e18b83ad8340587befb761122bef7baced0e8007
Presst0n <pr3sst0n@gmail.com>
2021-03-15 15:32:47 +0100

    Added PlayerClass model

---
+++ ResPublicaDashboard/Models/PlayerClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResPublicaDashboard.Models
+{
+    public class PlayerClass
+    {
+        public string ClassName { get; set; }
+        public List<string> Specializations { get; set; }
+    }
+}
